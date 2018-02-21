9d0f1a42e84198067e64c741cdd52ec1d417b369
MATT\Matthew <mpuent1@students.towson.edu>
2018-02-20 21:46:28 -0500

    Project setup

---
+++ src/main/java/net/ddns/hackstreetboys/ebookstack/EbookStackApplication.java
@@ -0,0 +1,12 @@
+package net.ddns.hackstreetboys.ebookstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookStackApplication.class, args);
+	}
+}
